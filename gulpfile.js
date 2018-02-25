1fed826ed372c65f6bf4b3815e817e722478dca4
zlatkopetkovski <zlatko_petkovski@hotmail.com>
2018-02-25 17:49:41 +0100

    change gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,25 @@
 var gulp = require('gulp');
 var mainBowerFiles = require('gulp-main-bower-files');
-
-gulp.task('main-bower-files', function(){
+// Include plugins
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+//var rename = require('gulp-rename');
+//Concat JS
+gulp.task('scripts', function() {
+    return gulp.src('src/js/*.js')
+      .pipe(concat('main.js'))
+      .pipe(gulp.dest('build/js'));
+});
+ //Minify JS
+/*gulp.task('minifyJs', function() {
+    return gulp.src('build/js/main.js')
+      .pipe(concat('main.min.js'))
+      .pipe(uglify())
+      .pipe(gulp.dest('build/js'));
+});*/
+/*gulp.task('main-bower-files', function(){
 	return fulp.src('./bower.json')
 	.pipe(mainBowerFiles())
-})
+});*/
 
-gulp.task('default', function() {
-  //console.log('hallo');
-});
+gulp.task('default', ['scripts', 'minifyJs']);

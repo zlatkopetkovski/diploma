ff3d96ea78488f8d17ee1149f817416845fc99a3
zlatkopetkovski <zlatko_petkovski@hotmail.com>
2018-03-14 04:21:21 +0100

    edit js, add images

--- build/js/main.js
+++ build/js/main.js
@@ -6345,26 +6345,38 @@ $('.carousel').carousel({
 function showHashTargetTab() {
     //// tab switch function. this is Bootstrap
     $('#myTab a[href="'+window.location.hash+'"]').tab('show');
-
-    $('.package .sub-menu li').removeClass('active open');
-    $('.package .sub-menu a[href*="'+window.location.hash+'"]').parent('li').addClass('active open');
-}
+ //   $('.package .sub-menu a').removeClass('active show open'); //originalna bese $('.package .sub-menu li').removeClass('active show open');
+ //   $('.package .sub-menu a[href*="'+window.location.hash+'"]').parent('li').addClass('active open');
+};
 $(document).on('click', '#myTab a[data-toggle="tab"]', function (e) {
     e.preventDefault();
     window.location.hash = $(this)[0].hash;
     //// tab switch function. this is Bootstrap again
     $(this).tab('show');
+    $
 });
-
 // this happens on load, we check if the hash exists and if it is show the specific tab, otherwise show default tab
 if(window.location.hash) {
     showHashTargetTab();
 } else {
     //// tab switch function. this is Bootstrap again
     $('#myTab a[data-toggle="tab"]:first').tab('show');
-}
+};
 // we need to know when we change the hash to show the specific tab
 $(window).on('hashchange', function(e) {
     e.preventDefault();
     showHashTargetTab();
 });
+//funkcija za postavuvanje na scrol koga odbirame drug tab
+// The function actually applying the offset
+function offsetAnchor() {
+    if(location.hash.length !== 0) {
+        window.scrollTo(window.scrollX, window.scrollY-5000);
+    }
+}
+// This will capture hash changes while on the page
+window.addEventListener("hashchange", offsetAnchor);
+// This is here so that when you enter the page with a hash,
+// it can provide the offset in that case too. Having a timeout
+// seems necessary to allow the browser to jump to the anchor first.
+window.setTimeout(offsetAnchor, 1); // The delay of 1 is arbitrary and may not always work right (although it did in my testing).

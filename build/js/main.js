d54639efec307441d9767132a9f7b587a2f86235
zlatkopetkovski <zlatko_petkovski@hotmail.com>
2018-03-15 13:16:42 +0100

    fix bug in tabs

--- build/js/main.js
+++ build/js/main.js
@@ -6345,15 +6345,12 @@ $('.carousel').carousel({
 function showHashTargetTab() {
     //// tab switch function. this is Bootstrap
     $('#myTab a[href="'+window.location.hash+'"]').tab('show');
- //   $('.package .sub-menu a').removeClass('active show open'); //originalna bese $('.package .sub-menu li').removeClass('active show open');
- //   $('.package .sub-menu a[href*="'+window.location.hash+'"]').parent('li').addClass('active open');
 };
 $(document).on('click', '#myTab a[data-toggle="tab"]', function (e) {
     e.preventDefault();
     window.location.hash = $(this)[0].hash;
     //// tab switch function. this is Bootstrap again
     $(this).tab('show');
-    $
 });
 // this happens on load, we check if the hash exists and if it is show the specific tab, otherwise show default tab
 if(window.location.hash) {
